daefbb32e0945f9f756d89067fc0b684dedc7abf
Valéry Nguyen <nguyen.valery1@gmail.com>
2026-02-09 09:25:56 +0100

    CR07

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ if h1:
 else:
     st.title("VOTRE LOGO")
 
-st.caption("Propulsé par EVERBOARDING · The Upskilling Company")
+st.caption("Propulsé par EVERBOARDING · Solutions d'Upskilling")
 
 user = st.session_state.get("user")
 
